e1bfcaa1f43fdeebc935a6887853026f46030b29
omaritooo <themedic227@gmail.com>
2022-04-09 14:55:45 +0200

    tailwind support

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import './index.css'
+
+
 Vue.use(axios)
 Vue.prototype.$username = 'hmaser'
 Vue.prototype.$password = 'hmaserv1987'
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="bg-red-400">
     <!-- <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />-->
     <div class="tabholder">
